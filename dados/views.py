3278c1b420b534a14a93ee6cfb4fa7526b57f3f4
Rafael Barbosa Ramos <141770380+RafaelADS7@users.noreply.github.com>
2025-08-23 12:29:45 -0300

    Adicionando autenticação na tela de login!

--- dados/views.py
+++ dados/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 def login_view(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        try:
+            user_obj = User.objects.get(email=email)
+            user = authenticate(request=request, username=user_obj.username, password=senha)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, 'Senha incorreta')
+        except User.DoesNotExist:
+            messages.error(request, 'Email não encontrado')
     return render(request, 'Telalogin.html')
+
+
+
+    
 
 def home_view(request):
     return render(request, 'Telahome.html')
